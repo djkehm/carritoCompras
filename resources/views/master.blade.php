f4a6dafe0ab3ce121ecdfdb6d5349c595b54d595
djkehm <kehmit.uribegqgmail.com>
2023-05-17 06:48:07 -0400

    Inicio del proyecto, se quito la pagina de bienvenida laravel y se puso una blade master con un navbar, sin contenidos aun

---
+++ resources/views/master.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>master</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="{{ asset('css/masterStyle.css') }}" rel="stylesheet">
+  </head>
+  <header>
+  <nav class="navbar bg-black text-white">
+    <div class="row justify-content-md-left">
+        <a class="navbar-brand  ps-4" href="#">
+            <img src="{{asset('images/pngwing.com.png')}}" alt="Bootstrap" width="30" height="24">
+        </a>
+    </div>
+    <div class="row justify-content-md-center">
+        <div class="col-md-auto text-white">
+            <a href="" class="boton">Inicio</a>
+            <a href="" class="boton">Quienes Somos</a>
+            <a href="" class="boton">Servicios</a>
+        </div>
+    </div>
+    <div class="row">
+        <a class="navbar-brand  pe-4" href="#">
+            <img src="{{asset('images/user.png')}}" alt="Bootstrap" width="30" height="24">
+        </a>
+    </div>
+  </nav>
+  </header>
+  <body class="">
+    <div class="container-fluid bg-container p-5">
+        <div class="row bg p-5">
+            <h1>Hello, world!</h1>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</html>
